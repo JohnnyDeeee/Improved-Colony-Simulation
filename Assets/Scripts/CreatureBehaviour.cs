9edbb5df910643b18749fd89f06e3db863452039
JohnnyDeeee <gamefake@live.nl>
2019-02-24 14:37:13 +0100

    Added basic creatures with basic physics and a seek function that makes them follow the mouse cursor

---
+++ Assets/Scripts/CreatureBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureBehaviour : ExposableMonobehaviour {
+    [ExposeProperty, SerializeField] public float mass { get; set; }
+    [ExposeProperty, SerializeField] public float maxSpeed { get; set; }
+    [ExposeProperty, SerializeField] public float maxForce { get; set; }
+    [ExposeProperty, SerializeField] public Vector2 acceleration { get; private set; }
+    [ExposeProperty, SerializeField] public Vector2 velocity { get; private set; }
+    [ExposeProperty, SerializeField] public Vector2 position { get; private set; }
+
+    // Start is called before the first frame update
+    void Awake() {
+        // Defaults
+        this.mass = 1f;
+        this.maxSpeed = 0.01f;
+        this.maxForce = 0.0005f;
+    }
+
+    void Start() {
+        this.position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Physics
+        this.velocity += this.acceleration;
+        this.velocity = Vector2.ClampMagnitude(this.velocity, this.maxSpeed); // Max speed
+        if (this.velocity.magnitude == 0)
+            this.velocity = Vector2.ClampMagnitude(this.velocity, this.maxSpeed / 10); // Min speed (to prevent creatures from being unable to move)
+        this.position += this.velocity;
+        this.acceleration *= 0; // Clear acceleration for the next frame (otherwise it will build up)
+
+        this.transform.position = this.position;
+
+        // Display
+        this.transform.localScale = new Vector3(this.mass, this.mass, 0) * 0.1f;
+    }
+
+    public void ApplyForce(Vector2 force) {
+        Vector2 _force = force / this.mass; // Size matters (bigger = slower, smaller = faster)
+        this.acceleration += _force;
+    }
+
+    public Vector2 SeekForce(Vector2 targetPosition) {
+        Vector2 direction = (targetPosition - this.position).normalized;
+        direction *= this.maxSpeed;
+        Vector2 steering = (direction - this.velocity).normalized;
+        steering = Vector2.ClampMagnitude(steering, this.maxForce);
+
+        return steering;
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+    private List<CreatureBehaviour> creatures = new List<CreatureBehaviour>();
+
+    // Start is called before the first frame update
+    void Start() {
+        // Spawn creature
+        for (int i = 0; i < 10; i++) {
+            CreatureBehaviour creature = GameObject.Instantiate<GameObject>(Resources.Load("Prefabs/creature") as GameObject).GetComponent<CreatureBehaviour>();
+            creature.mass = Random.Range(1f, 10f);
+            Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+            creature.transform.position = new Vector2(Random.Range(0f, screenBounds.x), Random.Range(0f, screenBounds.y));
+            this.creatures.Add(creature);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Let creature move
+        foreach (CreatureBehaviour creature in this.creatures) {
+            Vector2 force = creature.SeekForce(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            creature.GetComponent<CreatureBehaviour>().ApplyForce(force);
+        }
+    }
+}
